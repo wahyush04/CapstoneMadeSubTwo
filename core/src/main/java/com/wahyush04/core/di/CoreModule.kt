b84e34691793d329cf140fb9542e0a63b983b60a
wahyush04 <wahyush04@gmail.com>
2022-11-20 23:25:54 +0800

    database encryption & certificate pinning

--- core/src/main/java/com/wahyush04/core/di/CoreModule.kt
+++ core/src/main/java/com/wahyush04/core/di/CoreModule.kt
@@ -9,6 +9,9 @@ import com.wahyush04.core.data.source.remote.RemoteDataSource
 import com.wahyush04.core.data.source.remote.network.ApiService
 import com.wahyush04.core.domain.repository.INewsRepository
 import com.wahyush04.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,20 +23,34 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<GnewsDatabase>().gnewshDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("wahyush04".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             GnewsDatabase::class.java, "gnews.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 
 val networkModule = module {
     single {
+        val hostname = "gnews.io"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/NJRIFvUE0mycD8zAoo80ZwdxkgP/9Jo0h4f9bQJ+5KQ=")
+            .add(hostname, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .add(hostname, "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=")
+            .add(hostname, "sha256/NJRIFvUE0mycD8zAoo80ZwdxkgP/9Jo0h4f9bQJ+5KQ=")
+            .add(hostname, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .add(hostname, "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
